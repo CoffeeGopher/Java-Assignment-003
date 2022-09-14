9b5b2c4e1e81facfe024eef3f2f3b934d55d8746
Brennan <bduffy279@mycr.redwoods.edu>
2022-09-14 11:55:53 -0700

    Can now process metadata from URL links in addition to file paths.

--- src/HiddenSecrets.java
+++ src/HiddenSecrets.java
@@ -5,12 +5,10 @@ import com.drew.metadata.Metadata;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Tag;
 import com.drew.imaging.ImageMetadataReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
-import java.nio.file.Path;
+import java.io.*;
+
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -40,16 +38,48 @@ public class HiddenSecrets {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.print("File path to JPEG: ");
-        Scanner scanner = new Scanner(System.in);
-        Path filePath = Paths.get(scanner.nextLine());
+    public static void getHiddenSecretsFromURL(URL url) {
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(
+                    url.openStream()
+            );
+            for (Directory directory : metadata.getDirectories()) {
+                for (Tag tag : directory.getTags()) {
+                    System.out.format("[%s] - %s = %s%n",
+                            directory.getName(), tag.getTagName(), tag.getDescription());
+                }
+                if (directory.hasErrors()) {
+                    for (String error : directory.getErrors()) {
+                        System.err.format("ERROR: %s%n", error);
+                    }
+                }
+            }
+        } catch (ImageProcessingException e) {
+            System.out.println("Failed to process the image meta-data.");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Problem reading from URL.");
+            throw new RuntimeException(e);
+        }
+    }
 
-        getHiddenSecrets(filePath.toFile());
-        // Put your code to request a file path,
-        // read in a string from System.in,
-        // convert that string into A Path type using Paths class,
-        // and call the getHiddenSecrets method to get the file's meta-data
-        // HERE
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.print("File path or URL to image: ");
+            Scanner scanner = new Scanner(System.in);
+            String location = scanner.nextLine().trim();
+
+            // add a lazy exit clause
+            if (location.equalsIgnoreCase("end") || location.equalsIgnoreCase("exit")) {
+                return;
+            }
+
+            if (location.startsWith("https://") || location.startsWith("http://")) {
+                getHiddenSecretsFromURL(new URL(location));
+            } else {
+                getHiddenSecrets(Paths.get(location).toFile());
+            }
+            System.out.println();
+        }
     }
 }
